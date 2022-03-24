32ab2e809638c6a07de4188ca30f88401437fba1
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-03-24 12:05:34 -0400

    Page Layout Drafted

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './components/Home'
+import Form from './components/Form'
+import Confirm from './components/Confirm'
+import './styles/App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Nav />
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/confirm" element={<Confirm />} />
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/Form.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <input></input>
+        <input></input>
+        <input></input>
+        <input></input>
+        <button></button>
+      </form>
+    </div>
+  )
+}
+
+export default Form
