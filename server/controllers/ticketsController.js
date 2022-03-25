23166a6d19c5f72b7984242813b05dfc842f914d
LawrenceOL <loreganlloyd@gmail.com>
2022-03-25 14:42:59 -0400

    added more routes

--- server/controllers/ticketsController.js
+++ server/controllers/ticketsController.js
@@ -1,7 +1,6 @@
 const { Ticket } = require('../models')
 
 const getTicketCount = async (req, res) => {
-  console.log(req.body)
   try {
     const tickets = await Ticket.count()
     return res.status(200).json({ tickets })
@@ -10,6 +9,16 @@ const getTicketCount = async (req, res) => {
   }
 }
 
+const getId = async (req, res) => {
+  try {
+    const tickets = await Ticket.findOne({ sold_status: 'false' })
+    return res.status(200).json({ tickets })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  getTicketCount
+  getTicketCount,
+  getId
 }
